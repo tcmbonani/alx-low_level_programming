cf782396c86b5f6def845eb32bd3d9c15bf145a1
tcmbonani <tcmbonani@gmail.com>
2023-07-14 07:55:15 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,16 +4,16 @@
 *main - Entry point
 *Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
 char c;
 
 c = 'a';
-while 
+while
 (c <= 'z') {
 putchar(c);
 c++;
 }
 putchar('\n');
-return(0);
+return (0);
 }
