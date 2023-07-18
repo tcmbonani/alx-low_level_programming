adea7a08f470f73bf50d4e11de91c8273f29f411
tcmbonani <tcmbonani@gmail.com>
2023-07-18 00:57:32 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include <main.h>
 
 /**
- * * main - prints all possible combinations of two-digit numbers
- * *
- * * Return: Always 0 
- * */
+ *  main - prints all possible combinations of two-digit numbers
+ *
+ *  Return: Always 0 
+ */
 
 int main(void)
 {
