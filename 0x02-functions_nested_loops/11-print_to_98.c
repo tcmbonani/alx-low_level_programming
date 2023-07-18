5e2bdaf21026f4f04dd223ddbcece81735f8082a
tcmbonani <tcmbonani@gmail.com>
2023-07-18 11:55:01 -0700

    10-add.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
-* print_to_98 - Prints all natural numbers from n to 98 
+* print_to_98 - Prints all natural numbers from n to 98
 * @n: The number to start printing from
 * Return: Always 0.
 */
