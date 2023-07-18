1754d1c41ecdddbaa09c72777e75d6a5961b7a02
tcmbonani <tcmbonani@gmail.com>
2023-07-18 01:10:13 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
+
+
 /**
- *  main - Entry point of my code,
- *
- *  Return: Always 0 
- */
+ * * main - Entry point of my code, 
+ * *
+ * * Return: Always 0 
+ * */
 
 int main(void)
 {
