4426d0c1d4b108babe4dd6ae8624a3aa417d32d9
tcmbonani <tcmbonani@gmail.com>
2023-07-18 04:38:51 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 
 /**
  *  _islower - Checks for lowercase character
- *  @c: The character to be checked 
- *  Return: 1 for lowercase character or 0 for anything else 
+ *  @c: The character to be checked
+ *  Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
