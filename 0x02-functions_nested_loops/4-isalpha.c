46a338e758d5f68015a85f68312a677640e0e81b
tcmbonani <tcmbonani@gmail.com>
2023-07-18 05:47:39 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,11 @@
 /**
 * _isalpha - Checks for alphabetic character
 * @c: The character to be checked
-* Return: 1 for lowercase character or 0 for anything else 
+* Return: 1 for lowercase character or 0 for anything else
 */
 
 int _isalpha(int c)
  {
-
 if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
 {
 return (1);
