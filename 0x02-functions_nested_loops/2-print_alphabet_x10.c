f8be3b44e69c32cdd0c46bc6fb84e4ed788e38e0
tcmbonani <tcmbonani@gmail.com>
2023-07-18 04:11:13 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * * print_alphabet_x10 - Make alphabet x10 times 
- * * Return: void 
- * * 
- * */
+ *print_alphabet_x10 - Make alphabet x10 times
+ *Return: void
+ *
+*/
 
 void print_alphabet_x10(void)
 {
