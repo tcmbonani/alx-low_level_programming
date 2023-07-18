a1f87894fc17fdf1f17fdace26cbe3b8ade1a104
tcmbonani <tcmbonani@gmail.com>
2023-07-18 11:20:56 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* times_table -Prints the 9 times table, starting with 0 
+* times_table -Prints the 9 times table, starting with 0
 * Return: empty output
 */
 
@@ -19,13 +19,13 @@ z = x * y;
 if (z > 9)
 {
 u = z % 10;
-d = (z - u) / 10 ;
+d = (z - u) / 10;
 _putchar(44);
 _putchar(32);
 _putchar(d + '0');
 _putchar(u + '0');
 }
-else 
+else
 {
 if (y != 0)
 {
