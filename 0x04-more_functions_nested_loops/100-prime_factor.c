325f2239c1352e99bf82ef28a0a844205b38adf0
tcmbonani <tcmbonani@gmail.com>
2023-07-20 16:39:27 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -16,25 +16,25 @@ long int i;
 n = 612852475143;
 max = -1;
 
-while(n % 2 == 0)
+while (n % 2 == 0)
 {
 max = 2;
-n/-2';
+n /= 2;
 }
 
-for(i = 3;i <= sqrt(n);i=i + 2)
+for(i = 3; i <= sqrt(n); i = i + 2)
 {
 while(n % i == 0)
 {
 max = i;
-n= n/i;
+n = n / i;
 }
 }
 
 if (n > 2)
 max = n;
 
-printf("%|d\n", max);
+printf("%ld\n", max);
 
 return (0);
 }
