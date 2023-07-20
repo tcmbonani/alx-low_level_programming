c4d60810c66aae6287dddc6159bbc6b93c8cf452
tcmbonani <tcmbonani@gmail.com>
2023-07-20 16:15:37 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,16 +17,17 @@ else
 
 int i, j;
 
-for (i = 1 ; i < size; i++)
+for (i = 1 ; i <= size; i++)
 {
 for (j = i; j < size; j++)
 {
 putchar(' ');
 }
 for (j = 1; j <= i; j++)
+{
 putchar('#');
 }
 putchar('\n');
-
+}
 }
 }
