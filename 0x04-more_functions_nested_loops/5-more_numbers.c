20a9542e2c3079c45ab915148491ac9507661901
tcmbonani <tcmbonani@gmail.com>
2023-07-20 14:27:26 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,11 @@ void more_numbers(void)
 {
 int i,j;
 
-for (i = 1; i <= 14; i++)
+for (i = 1; i <= 10; i++)
 {
-for (j = 0; i <= 14; j++)
+for (j = 0; j <= 14; j++)
 {
-if(j >=10)
+if(j >= 10)
 putchar('1');
 putchar(j % 10 + '0');
 }
