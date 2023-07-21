d80b61ae00d25600ff667ea50a9279adfea77c28
tcmbonani <tcmbonani@gmail.com>
2023-07-20 17:11:33 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,10 +15,10 @@ if (n < 0)
 {
 n *= -1;
 k = n;
-_putchar('-');
+putchar('-');
 }
 k /= 10;
 if (k != 0)
 print_number(k);
-_putchar((unsigned int) n % 10 + '0');
+putchar((unsigned int) n % 10 + '0');
 }
