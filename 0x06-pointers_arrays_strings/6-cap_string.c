2c45cee4e31454f736d242d073e54d6e4af2af82
tcmbonani <tcmbonani@gmail.com>
2023-07-26 12:01:38 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -25,8 +25,8 @@ str[index - 1] == '?' ||
 str[index - 1] == '"' ||
 str[index - 1] == '(' ||
 str[index - 1] == ')' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
 index == 0)
 str[index] -= 32;
 index++;
