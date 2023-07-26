1ae18f352a8a9e139765f327356a2ad5ba2d87e8
tcmbonani <tcmbonani@gmail.com>
2023-07-26 06:29:43 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
 * _strcmp - compare string value
 * @s1: input value
 * @s2: input value
-* Return: s1[i] = s2[i]
+* Return: s1[i] - s2[i]
 */
 
 char _strcmp(char *s1, char *s2)
@@ -14,9 +14,9 @@ int i;
 i = 0;
 while (s1[i] != '\0' && s2[i] != '\0')
 {
-if (s1[i] != s2[i] );
+if (s1[i] != s2[i])
 {
-return (s1[i] = s2[i])
+return (s1[i] - s2[i]);
 }
 i++;
 }
