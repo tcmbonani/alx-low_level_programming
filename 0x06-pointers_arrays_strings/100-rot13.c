baf78a2772526ae400efba45f59511c5ff7a1c20
tcmbonani <tcmbonani@gmail.com>
2023-07-27 11:54:51 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * rot13 - encoder rot13
-* @s: 
-* Return: encoded string
+* @s: pointer to string params
+* Return: *s
 */
 
+char *rot13(char *s)
 {
 int i;
 int j;
