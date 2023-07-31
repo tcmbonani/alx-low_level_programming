7e07ff3a4d83363d55121aa5769c342dc3f446cc
tcmbonani <tcmbonani@gmail.com>
2023-07-31 05:27:11 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,11 +6,11 @@
  * @c: input
  * Return: Always 0 (Success)
  */
-char *_strchr(char *s, char c
+char *_strchr(char *s, char c)
 {
 int i = 0;
 
-for (; s[i] >= '/0'; i++)
+for (; s[i] >= '\0'; i++)
 {
 if(s[i] == c)
 return (&s[i]);
