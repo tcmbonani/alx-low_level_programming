ef7dfded75f30870d469170d6c1f48998aa47c9d
tcmbonani <tcmbonani@gmail.com>
2023-08-02 12:11:12 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
 /**
 *wildcmp - Compare strings
 *@s1: pointer to string params
-*@s2: pointer to string params 
+*@s2: pointer to string params
 *Return: 0
 */
 
@@ -13,18 +13,18 @@ if (*s1 == '\0')
 {
 if (*s2 != '\0' && *s2 == '*')
 {
-return(wildcmp(s1,s2 + 1));
+return (wildcmp(s1, s2 + 1));
 }
-return(*s2 == '\0');
+return (*s2 == '\0');
 }
 
 if(*s2 == '*')
 {
-return (wildcmp(s1 + 1,s2) || wildcmp(s1, s2 + 1));
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
 else if (*s1 == *s2)
 {
-return(wildcmp(s1 + 1,s2 + 1));
+return (wildcmp(s1 + 1, s2 + 1));
 }
 return (0);
 }
