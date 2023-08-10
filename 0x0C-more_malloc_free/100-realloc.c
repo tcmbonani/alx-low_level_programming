28a383f405613e991ba30145bce2b8e0ee8b5437
tcmbonani <tcmbonani@gmail.com>
2023-08-10 11:27:32 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,45 +1,69 @@
-#include "main.h"
 #include <stdlib.h>
 
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 /**
-* _realloc - reallocates a memory block
-* @ptr: pointer to the memory previously allocated with a call to malloc
-* @old_size: size of ptr
-* @new_size: size of the new memory to be allocated
-*
-* Return: pointer to the address of the new memory block
-*/
-
+ * _realloc - allocates memory block using malloc and free
+ * @ptr: pointer to the memory previosly allocated with malloc
+ * @old_size: The size of the allocated space of ptr
+ * @new_size: The new size to allocate
+ *
+ * Description: allocates a new memory block for the pointer,
+ * using the contents from the original pointer, copiyng up to the
+ * minimum of the old and new sizes.
+ * If new_size > old_size, the added memory should not be intialized
+ * If new_size == old_size, returns the same pointer
+ * If ptr == NULL, call is equivalent to malloc(new_size)
+ * If new_size == 0 and ptr != NULL, call is equivalent to free(ptr),
+ *  and return NULL.
+ *
+ * Return: A pointer to the new allocated memory and free ptr.
+ * NULL if can not allocate memory
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *temp_block;
-	unsigned int i;
+	char *p;
 
-	if (ptr == NULL)
-	{
-		temp_block = malloc(new_size);
-		return (temp_block);
-	}
-	else if (new_size == old_size)
+	if (new_size == old_size)
 		return (ptr);
 
-	else if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else
+
+	p = malloc(new_size);
+
+	if (p == NULL)
+		return (NULL);
+
+	if (ptr == NULL)
+		return (p);
+
+	p = _memcpy(p, ptr, (new_size > old_size ? old_size : new_size));
+	free(ptr);
+	return (p);
+}
+
+/**
+ * _memcpy - copies the memory are from
+ * src to dest
+ * @dest: The destination pointer
+ * @src: The source pointer
+ * @n: bytes to use from src
+ *
+ * Return: The pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		temp_block = malloc(new_size);
-		if (temp_block != NULL)
-		{
-			for (i = 0; i < min (old_size, new_size); i++)
-				*((char *)temp_block + i) = *((char *) ptr + i);
-			free(ptr);
-			return (temp_block);
-		}
-		else
-			return (NULL);
+		*(dest + i) = *(src + i);
+		i++;
 	}
+
+	return (dest);
 }
